c5e9b4ccb32ed8fbe0d24ef00e7381fdc1f58574
alan Dittler <dittler.a@gmail.com>
2020-12-01 21:55:53 -0300

    update

--- src/job.js
+++ src/job.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { Card, Badge } from "react-bootstrap";
+import React, { useState } from "react";
+import { Card, Badge, Button, Collapse } from "react-bootstrap";
+import ReactMarkdown from "react-markdown";
 
-export default function job({ job }) {
+export default function Job({ job }) {
+  const [open, setOpen] = useState(false);
   return (
-    <Card>
+    <Card className="mb-3">
       <Card.Body>
         <div className="d-flex justify-content-between">
           <div>
@@ -16,10 +18,31 @@ export default function job({ job }) {
             <Card.Subtitle className="text-muted mb-2">
               {new Date(job.created_at).toLocaleDateString()}
             </Card.Subtitle>
-            <Badge variant="secondary">{job.type}</Badge>
+            <Badge variant="secondary" className="mr-2">
+              {job.type}
+            </Badge>
             <Badge variant="secondary">{job.location}</Badge>
+            <div>
+              <div style={{ wordBreak: "break-all" }}>
+                <ReactMarkdown source={job.how_to_apply} />
+              </div>
+            </div>
           </div>
+          <img height="50" src={job.company_logo} alt={job.company} />
         </div>
+        <Card.Text>
+          <Button
+            onClick={() => setOpen((prevOpen) => !prevOpen)}
+            variant="primary"
+          >
+            {open ? "Hide Details" : "View Details"}
+          </Button>
+        </Card.Text>
+        <Collapse in={open}>
+          <div className="mt-4">
+            <ReactMarkdown source={job.description} />
+          </div>
+        </Collapse>
       </Card.Body>
     </Card>
   );
