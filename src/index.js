6426388f6eb900d97c2d7f47aa6ae06f2781e7f0
alan Dittler <dittler.a@gmail.com>
2020-12-20 20:45:31 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-import TimeAgo from "javascript-time-ago";
-
-import en from "javascript-time-ago/locale/en";
-import ru from "javascript-time-ago/locale/ru";
-
-TimeAgo.addDefaultLocale(en);
-TimeAgo.addLocale(ru);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import HomePage from './components/HomePage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  <Provider store={store}>
+    <HomePage />
+  </Provider>,
+  document.getElementById('root')
 );
